2a47db1723c6c837ef91662f370282a1721db472
syphua2 <syphua2@gmail.com>
2017-11-28 22:33:17 +0700

    get loading

---
+++ wp-content/themes/construction-child/content/js/pages/loading.js
@@ -0,0 +1,9 @@
+'use strict';
+(function($, window, document, undefined) {
+    $(document).ready(function () {
+        $('#lightbox').show();
+        $('#loading').show();
+
+        $('.js-select2').select2();
+    });
+})(window.jQuery, window, document);
